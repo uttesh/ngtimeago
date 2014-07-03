508da32dfcd34157b210776800261e3ea1fb3e0d
uttesh <uttesh@gmail.com>
2014-07-04 00:10:50 +0530

    Update ngtimeago.js

--- ngtimeago.js
+++ ngtimeago.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var myapp = angular.module('ngTimeago', []);
+var myapp = angular.module('ngtimeago', []);
 
 myapp.filter('timeago', function() {
         return function(input, p_allowFuture) {
