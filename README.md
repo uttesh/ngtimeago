63bc0bfe57f5d1b07798019bee6a518b64607969
androkumar04 <androkumar04@gmail.com>
2015-06-05 10:00:39 +0530

    First pages commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# ng-timeago
+
+Is an AngularJS module for simple time ago on static data. No directives here, just a filters.
+
+Mostly based on various snippets which I found on JSFiddle, with some changes by me.
+
+## Quick start
+
+```
+bower install ng-timeago
+```
+new version
+```
+bower install ng-timeago0.0.2
+```
+
+or alternatively download and include `ngtimeago.js` after `angular.min.js`.
+
+Add the `ngtimeago` module as a dependency when creating your app, e.g.
+
+```
+var app = angular.module('myApp', ['ngTimeago']);`
+```
+
+NO NEED TO INJECT in controller directly use in html.
+
+## Rendering
+
+
+```
+                                <p>
+                                    
+                                    <small>
+
+                                        posted {{createdOn|timeago}} by uttesh
+
+
+                                    </small>
+                                </p>
+```
+
+
+## Contributions
+
+For problems/suggestions please create an issue on Github.
+
+## Contributors
+
+* [@uttesh](https://twitter.com/uttesh)
+
+## Credits
+
+* Other uknown JSFiddles
---
+++ ngtimeago.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var catalyst = angular.module('ngtimeago', []);
+
+
+catalyst.filter('timeago', function() {
+        return function(input, p_allowFuture) {
+		
+            var substitute = function (stringOrFunction, number, strings) {
+                    var string = angular.isFunction(stringOrFunction) ? stringOrFunction(number, dateDifference) : stringOrFunction;
+                    var value = (strings.numbers && strings.numbers[number]) || number;
+                    return string.replace(/%d/i, value);
+                },
+                nowTime = (new Date()).getTime(),
+                date = (new Date(input)).getTime(),
+                //refreshMillis= 6e4, //A minute
+                allowFuture = p_allowFuture || false,
+                strings= {
+                    prefixAgo: '',
+                    prefixFromNow: '',
+                    suffixAgo: "ago",
+                    suffixFromNow: "from now",
+                    seconds: "less than a minute",
+                    minute: "about a minute",
+                    minutes: "%d minutes",
+                    hour: "about an hour",
+                    hours: "about %d hours",
+                    day: "a day",
+                    days: "%d days",
+                    month: "about a month",
+                    months: "%d months",
+                    year: "about a year",
+                    years: "%d years"
+                },
+                dateDifference = nowTime - date,
+                words,
+                seconds = Math.abs(dateDifference) / 1000,
+                minutes = seconds / 60,
+                hours = minutes / 60,
+                days = hours / 24,
+                years = days / 365,
+                separator = strings.wordSeparator === undefined ?  " " : strings.wordSeparator,
+            
+               
+                prefix = strings.prefixAgo,
+                suffix = strings.suffixAgo;
+                
+            if (allowFuture) {
+                if (dateDifference < 0) {
+                    prefix = strings.prefixFromNow;
+                    suffix = strings.suffixFromNow;
+                }
+            }
+
+            words = seconds < 45 && substitute(strings.seconds, Math.round(seconds), strings) ||
+            seconds < 90 && substitute(strings.minute, 1, strings) ||
+            minutes < 45 && substitute(strings.minutes, Math.round(minutes), strings) ||
+            minutes < 90 && substitute(strings.hour, 1, strings) ||
+            hours < 24 && substitute(strings.hours, Math.round(hours), strings) ||
+            hours < 42 && substitute(strings.day, 1, strings) ||
+            days < 30 && substitute(strings.days, Math.round(days), strings) ||
+            days < 45 && substitute(strings.month, 1, strings) ||
+            days < 365 && substitute(strings.months, Math.round(days / 30), strings) ||
+            years < 1.5 && substitute(strings.year, 1, strings) ||
+            substitute(strings.years, Math.round(years), strings);
+			console.log(prefix+words+suffix+separator);
+			prefix.replace(/ /g, '')
+			words.replace(/ /g, '')
+			suffix.replace(/ /g, '')
+			return (prefix+' '+words+' '+suffix+' '+separator);
+            
+        };
+    });
+
+    
