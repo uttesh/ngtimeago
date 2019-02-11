6f6561020d997054c4c39ce74c9576aaa7089854
Kimsan Lim <kimsanlim27@gmail.com>
2019-02-11 18:29:12 +0700

    [Finish] Translate Khmer and Vietnamese language

--- ngtimeago.js
+++ ngtimeago.js
@@ -2,36 +2,72 @@
 
 var catalyst = angular.module('ngtimeago', []);
 
+var getTranslateTime = function() {
+  var language = localStorage['current_language'] == undefined ? 'en' : localStorage['current_language'];
+  var strings = {
+      prefixAgo: '',
+      prefixFromNow: '',
+      suffixAgo: "ago",
+      suffixFromNow: "from now",
+      seconds: "~ a min",
+      minute: "a min",
+      minutes: "%d mins",
+      hour: "1 hour",
+      hours: "%d hours",
+      day: "1 day",
+      days: "%d days",
+      month: "1 month",
+      months: "%d months",
+      year: "1 year",
+      years: "%d years"
+  }
+  if(language == 'kh') {
+    strings.suffixAgo = 'មុន'
+    strings.suffixFromNow = 'មុន'
+    strings.seconds = '~ មួយនាទី'
+    strings.minute = 'មួយនាទី'
+    strings.minutes = '%d នាទី'
+    strings.hour = '1 ម៉ោង'
+    strings.hours = '%d ម៉ោង'
+    strings.day = '1 ថ្ងៃ'
+    strings.days = '%d ថ្ងៃ'
+    strings.month = '1 ខែ'
+    strings.months = '%d ខែ'
+    strings.year = '1 ឆ្នាំ',
+    strings.years = '%d ឆ្នាំ'
+  }
+  else if(language == 'vn') {
+    strings.suffixAgo = 'trước'
+    strings.suffixFromNow = 'trước'
+    strings.seconds = '~ một phút'
+    strings.minute = 'một phút'
+    strings.minutes = '%d phút'
+    strings.hour = '1 giờ'
+    strings.hours = '%d giờ'
+    strings.day = '1 ngày'
+    strings.days = '%d ngày'
+    strings.month = '1 tháng'
+    strings.months = '%d tháng'
+    strings.year = '1 năm',
+    strings.years = '%d năm'
+  }
+
+  return strings;
+}
 
 catalyst.filter('timeago', function() {
         return function(input, p_allowFuture) {
-
             var substitute = function (stringOrFunction, number, strings) {
                     var string = angular.isFunction(stringOrFunction) ? stringOrFunction(number, dateDifference) : stringOrFunction;
                     var value = (strings.numbers && strings.numbers[number]) || number;
                     return string.replace(/%d/i, value);
                 },
+                currentLanguage = localStorage['current_language'] == undefined ? "en" : localStorage['current_language'],
                 nowTime = (new Date()).getTime(),
                 date = (new Date(input)).getTime(),
                 //refreshMillis= 6e4, //A minute
                 allowFuture = p_allowFuture || false,
-                strings= {
-                    prefixAgo: '',
-                    prefixFromNow: '',
-                    suffixAgo: "ago",
-                    suffixFromNow: "from now",
-                    seconds: "~ a min",
-                    minute: "a min",
-                    minutes: "%d mins",
-                    hour: "1 hour",
-                    hours: "%d hours",
-                    day: "1 day",
-                    days: "%d days",
-                    month: "1 month",
-                    months: "%d months",
-                    year: "1 year",
-                    years: "%d years"
-                },
+                strings= getTranslateTime(),
                 dateDifference = nowTime - date,
                 words,
                 seconds = Math.abs(dateDifference) / 1000,
@@ -63,10 +99,13 @@ catalyst.filter('timeago', function() {
             days < 365 && substitute(strings.months, Math.round(days / 30), strings) ||
             years < 1.5 && substitute(strings.year, 1, strings) ||
             substitute(strings.years, Math.round(years), strings);
-			console.log(prefix+words+suffix+separator);
+			// console.log(prefix+words+suffix+separator);
 			prefix.replace(/ /g, '')
 			words.replace(/ /g, '')
 			suffix.replace(/ /g, '')
+      if(currentLanguage == 'kh')
+        return (prefix+' '+words+''+suffix+' '+separator);
+
 			return (prefix+' '+words+' '+suffix+' '+separator);
 
         };
